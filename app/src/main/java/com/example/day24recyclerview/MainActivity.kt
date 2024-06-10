6b8ed4217ae3c73b4ee0cfbb257ca0a4d879b4d2
pradumkumar6 <pradumjaiswar790@gmail.com>
2024-06-11 02:54:25 +0530

    Day24 RecyclerView News App

---
+++ app/src/main/java/com/example/day24recyclerview/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.day24recyclerview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var myRecyclerView: RecyclerView
+    private lateinit var newsArrayList: ArrayList<News>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        myRecyclerView = findViewById(R.id.recyclerView)
+        // Create an image array
+        val newsImageArray = arrayOf(
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7,
+            R.drawable.img8,
+            R.drawable.img9,
+//            R.drawable.img10
+        )
+        // Create a news Heading array
+        val newsHeadingArray = arrayOf(
+            "U.K. Foreign Secretary James Cleverly raises issue of BBC tax searches with EAM Jaishankar",
+            "Cooking gas prices hiked by ₹50 for domestic, ₹350.50 for commercial cylinders",
+            "Joe Biden appoints two prominent Indian-American corporate leaders to his Export Council",
+            "Sergey Lavrov will raise suspected bombing of Nord Stream II at G20: Russian Foreign Ministry",
+            "Belarusian leader Lukashenko visits China amid Ukraine tensions",
+            "China rips new U.S. House committee on countering Beijing",
+            "It has been a disappointing T20 World Cup 2024 campaign for the Pakistan cricket team till now as the Babar Azam-led side slumped to a 6-run loss against India.",
+            "South Africa defeated Bangladesh by just four runs in another low-scoring game at the T20 World Cup on Monday",
+            "T20 World Cup 2024: Matthew Wade Reprimanded For Showing Dissent Following Umpire's Decision",
+            "Tried to develop PMO as catalytic agent’: PM Modi addresses officials on Day 1"
+        )
+        // To set items inside recycler view
+        myRecyclerView.layoutManager = LinearLayoutManager(this)
+        newsArrayList = arrayListOf<News>()
+        for (index in newsImageArray.indices) {
+            val news = News(newsHeadingArray[index], newsImageArray[index])
+            newsArrayList.add(news)
+        }
+        myRecyclerView.adapter = MyAdapter(newsArrayList, this)
+    }
+}
---
+++ app/src/main/java/com/example/day24recyclerview/MyAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.day24recyclerview
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+
+class MyAdapter(var newsArrayList: ArrayList<News>, var context: Activity) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    // To create a new view instance
+    //When layout manager fails to find the suitable view for each item
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.each_row, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    //Populates items with data
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = newsArrayList[position]
+        holder.hTitle.text = currentItem.newsHeading
+        holder.hImage.setImageResource(currentItem.newsImage)
+    }
+
+    // How many list items are present in your array
+    override fun getItemCount(): Int {
+        return newsArrayList.size
+    }
+
+    // It holds the view so that views are not created everytime. So memory can be saved
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val hTitle = itemView.findViewById<TextView>(R.id.headingTitle)
+        val hImage = itemView.findViewById<ShapeableImageView>(R.id.headingImage)
+    }
+}
